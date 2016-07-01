8400add2eaccb0ca0cd1e14d7e194b18f1d757eb
James Watson <james.r.watson@utah.edu>
2016-07-01 10:11:14 -0600

    added detail to decimal formatting

--- Learning/format-decimals.py
+++ Learning/format-decimals.py
@@ -2,5 +2,6 @@
 # -*- coding: utf-8 -*-
 from math import pi
 
-print pi          # 3.14159265359
-print "%.2f" % pi # 3.14
+print pi          #          3.14159265359
+print "%.2f" % pi #          3.14
+print "{0:.2f}".format(pi) # 3.14
