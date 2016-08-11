8772b2a44c652a974d5d8b3a6db36320a9ca811d
James Watson <james.r.watson@utah.edu>
2016-08-10 18:52:10 -0600

    arguments and global vars

---
+++ Learning/global_vars.py
@@ -0,0 +1,15 @@
+x = None
+y = None
+
+def change_x( nuX ):
+    """ The function that changes x """
+    # put a comment in there
+    # another
+    global x , y
+    x = nuX
+    y = nuX
+    
+change_x( 4 )    
+    
+print x
+print y
--- Learning/unpack-list-for-args.py
+++ Learning/unpack-list-for-args.py
@@ -9,3 +9,8 @@ fourArgFunc(*foo) # Found 4 args: 1 2 3 4
 fourArgFunc(*[5,6,7,8]) # Found 4 args: 5 6 7 8
 
 fourArgFunc('apple',*[9,10,11]) # Found 4 args: apple 9 10 11
+
+# fourArgFunc(*[12,13] , *[14,15]) # ERROR: invalid syntax
+# fourArgFunc(*[12,13] + *[14,15]) # ERROR: invalid syntax
+fourArgFunc(*[12,13] + [14,15]) # Found 4 args: 12 13 14 15
+fourArgFunc( *([12,13] + [14,15]) ) # Found 4 args: 12 13 14 15
