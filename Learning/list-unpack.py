35e3fcb15fde42ad9789867d883cffe4f0163011
jwatson_utah_edu <james.r.watson@utah.edu>
2017-03-14 22:22:32 -0600

    unpack lists , examples

---
+++ Learning/list-unpack.py
@@ -0,0 +1,17 @@
+"""
+RESULT: You can unpack a list into a sequence of variables separated by commas without a special operator
+"""
+
+foo , bar , baz = [ 1 , 2 , 3 ]
+print foo , bar , baz # 1 2 3
+[ foo , bar , baz ] = [ 4 , 5 , 6 ]
+print foo , bar , baz # 4 5 6
+
+def get_those_nums():
+    """ Return a list of numbers """
+    return [ 7 , 8 , 9 ]
+
+foo , bar , baz = get_those_nums()
+print foo , bar , baz # 7 8 9
+[ foo , bar , baz ] = get_those_nums()
+print foo , bar , baz # 7 8 9
