a0589913b9d5dcb4ab3445aad690ce5ec721c158
James Watson <james.watson-2@colorado.edu>
2019-02-28 22:28:48 -0700

    KB

--- Learning/Tkinter-Canvas/kb_input.py
+++ Learning/Tkinter-Canvas/kb_input.py
@@ -1,22 +1,25 @@
+# URL , keyboard and mouse input: http://effbot.org/tkinterbook/tkinter-events-and-bindings.htm
+# URL , kb event symbols: https://docstore.mik.ua/orelly/other/python/0596001886_pythonian-chp-16-sect-9.html
+
 from Tkinter import *
 
 root = Tk()
 
-def key(event):
+def key( event ):
     print "event.char:" , repr( event.char ) , "event.keysym:" , event.keysym
     if event.keysym in ( "Up" , "Down" , "Left" , "Right" ):
 		print "ARROWED!"
 
-def callback(event):
+def callback( event ):
     frame.focus_set()
-    print "clicked at", event.x, event.y
+    print "clicked at" , event.x , event.y
 
 # Tkinter window
-frame = Frame( root , width = 100 , height = 100 )
+frame = Frame( root , width = 300 , height = 300 )
 
 # Bind callbacks to specific types of events
-frame.bind("<Key>", key)
-frame.bind("<Button-1>", callback)
+frame.bind( "<Key>"      , key      )
+frame.bind( "<Button-1>" , callback )
 
 # Prepare window
 frame.pack()
