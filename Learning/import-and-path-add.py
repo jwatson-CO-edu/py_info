e38163ef201cbb6faf42a9228190603fe4dc5c71
James Watson <james.watson-2@colorado.edu>
2019-08-28 21:04:03 -0600

    Need to streamline the template

---
+++ Learning/import-and-path-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Template Version: 2017-04-13
+
+# ~~ Future First ~~
+from __future__ import division # Future imports must be called before everything else, including triple-quote docs!
+
+from test_pkg.report_file import what_I_got
+
+what_I_got( __file__ )
+
+
+"""
+RESULT: 
+"""
